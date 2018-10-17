f17dea3965f5b9e18f53f4d677886e10cf07615c
Marie Burns <mcburns2222@gmail.com>
2018-10-17 08:59:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(...array, );
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift();
+}
